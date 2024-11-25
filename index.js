f0a1d68e5c44cba80c0094b1debcbcd1a4a7c0e5
Placido <plajunpar@alu.edu.gva.es>
2024-11-25 19:09:17 +0100

    index.js hecho

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-//hola
+require("dotenv").config()
+const methodOverride = require("method-override")
+const express = require("express")
+const app = express()
+const path = require("path")
+const port = process.env.PORT || process.env.PUERTO
+const companyRoutes = require("./router/company.routes")
+const usersRoutes = require("./router/users.routes")
+const inscriptionRoutes = require("./router/inscription.routes")
+
+app.use(methodOverride("_method"))
+app.set("view engine","ejs")
+app.use(express.static(path.join(__dirname,"public")))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+app.use((req,res,next) => {
+    //Variables Globales
+})
+
+app.use(`/api/${process.env.API_VERSION}/company`)
+app.use(`/api/${process.env.API_VERSION}/users`)
+app.use(`/api/${process.env.API_VERSION}/inscription`)
+
+
+app.get("*",(req,res)=>{
+    res.status(500).json({err:"No existe la ruta"})
+})
+
+app.listen(port, async()=>{
+    console.log(`${process.env.MENSAJE} http://localhost:${port}/api/${process.env.API_VERSION}/company`)
+    try {
+        //Una vez levantado el servidor, intentamos conectar con MongoDB
+        await mongodbConfig.conectarMongoDB()
+        .then(()=>{
+            console.log("Conectado con MongoDB!!!")
+        })
+        .catch((err)=>{
+            //Si no conectamos con MongoDB, debemos tumbar el server
+            console.log(`Error al conectar. Desc: ${err}`)
+            process.exit(0)
+        })
+    } catch (error) {
+        //Si no conectamos con MongoDB, debemos tumbar el server
+        console.log(`Error en el server. Desc: ${error}`)
+        process.exit(0)
+    }
+})
