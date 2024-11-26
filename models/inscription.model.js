9ef1e6a9916b55696bddef13be7c82757777ba78
Jesus Hernández Mencía <jeshermen@alu.edu.gva.es>
2024-11-26 20:48:54 +0100

    Se ha añadido un constructor en el modelo de inscription

--- models/inscription.model.js
+++ models/inscription.model.js
@@ -1,3 +1,27 @@
 const mongoose = require("mongoose")
 
+const mongoose = require("mongoose")
+const { type } = require("os")
+
+const inscription = new mongoose.Schema({
+    IdUser:{
+        type:Number,
+    },
+    IdCompany:{
+        type:Number,
+    },
+    FecIni:{
+        type:Date,
+        require:true
+    },
+    FecFin:{
+        type:Date,
+    },
+    Observaciones:{
+        type:text
+    }
+})
+
+module.exports = inscription
+
 module.exports = comment
