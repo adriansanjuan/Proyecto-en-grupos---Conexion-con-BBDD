a5dacfccfa2634d2e34f2a8e306e276aebfaef47
Adrian Sanjuan Plaza <adrsanpla@alu.edu.gva.es>
2024-11-27 19:04:50 +0100

    se ha hecho el schema del company y se ha añadido la función de createCompany

--- models/company.model.js
+++ models/company.model.js
@@ -1,3 +1,80 @@
 const mongoose = require("mongoose")
 
+const companySchema = new mongoose.Schema({
+    //_id --> NO es String, sino ObjectID
+    cif:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    city:{
+        type:String,
+        required:true
+    },
+    personInCharge:{
+        type:String,
+        required:true
+    },
+    personInChargeID:{
+        type:String,
+        required:true
+    },
+    type:{
+        type:String,
+        required:false
+    },
+    family:{
+        type:String,
+        required:false
+    },
+    address:{
+        type:String,
+        required:false
+    },
+    area:{
+        type:String,
+        required:false
+    },
+    postalCode:{
+        type:Number,
+        required:false
+    },
+    phone:{
+        type:Number,
+        required:false
+    },
+    email:{
+        type:String,
+        required:false
+    },
+    createdDate:{
+        type:Date,
+        required:false
+    },
+    modifiedDate:{
+        type:Date,
+        required:false
+    }
+})
+
+//Crear modelo a partir del esquema MONGOOSE
+const company = mongoose.model("company", companySchema)
+
+company.createCompany = async(companyData, result) => {
+    const newCompany = new company(companyData)
+    await newCompany.save()
+    .then((datos) => {
+        result(null, datos)
+    })
+    .catch((err) => {
+        result(err, null)
+    })
+}
+
+company.findAll
+
 module.exports = comment
