606f72974cd6cf2e61eeced86fa7f5d4ea3b61a7
Placido <plajunpar@alu.edu.gva.es>
2024-11-27 19:06:35 +0100

    Hecho user models y cambiado .env

--- models/users.model.js
+++ models/users.model.js
@@ -1,3 +1,17 @@
-const mongoose = require("mongoose")
+const dbConn = require("../utils/mysql.config")
+const mysql = require("mysql")
+
+let user = function(empleado){
+    this.nif = empleado.nif
+    this.username = empleado.username
+    this.password = empleado.password
+    this.firstName = empleado.firstName
+    this.lastName = empleado.lastName
+    this.createdDate = new Date()
+    this.modifiedDate = empleado.modifiedDate
+    this.email = empleado.email
+    this.picture = empleado.picture
+    this.profile = empleado.profile
+}
 
 module.exports = comment
