d2fdf53d7847e280a8784f6bc197fcddbaa39b49
Adrian Sanjuan Plaza <adrsanpla@alu.edu.gva.es>
2024-11-29 15:47:19 +0100

    se ha avanzado con el controller ce companies

--- controllers/company.controller.js
+++ controllers/company.controller.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 const companyModel = require("../models/company.model")
 
-exports.findAllComments = async(req,res) => {
+exports.findAllCompaniesCSR = async(req,res) => {
     await companyModel.findAll({},function(err, datosComany){
         if(err){
             res.status(500).json({"err":err})
@@ -9,4 +9,19 @@ exports.findAllComments = async(req,res) => {
             res.status(200).json(datosCompany)
         }
     })
+}
+
+exports.findAllCompaniesSSR = async(req, res) => {
+    await companyModel.findAll(filtro, function(err, datosCompany){
+        if(err){
+            console.log(err)
+            res.render("error.ejs", {err:err.error})
+        }else{
+            res.render("../views/company/index.ejs", {comentarios:datosCompany})
+        }
+    })
+}
+
+exports.showNewCompany = async(req, res) => {
+    const {id} = req.params
 }
